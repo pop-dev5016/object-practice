d390347bf2e704fb70535d45f31fff1641e31825
devpop <impopcity@gmail.com>
2022-09-19 22:01:35 +0530

    method

---
+++ method .js
@@ -0,0 +1,21 @@
+let aliens = {
+    name : "pop",
+    regNo : 2012505016,
+    depatment:"instrumentation engineer",
+    cgpa:7.9,
+    'campus interview':"failed",
+    friend:{
+        name : "prem",
+        regNo : 2012505017,
+        depatment:"instrumentation engineer",
+        cgpa:9.9,
+        'campus interview': "twitter"
+    },
+    krithi:function(){
+        console.log("i m placed")
+    }
+
+};
+
+aliens.krithi()
+
