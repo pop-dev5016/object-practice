fe8d31d3c64bf5271d3c9a8733ca00e2559bfbca
devpop <impopcity@gmail.com>
2022-09-19 21:54:30 +0530

    version2

---
+++ arrow function.js
@@ -0,0 +1,57 @@
+// let geeet = function(){
+//     console.log("im pop")
+//     return 1000
+// }
+
+// console.log(geeet())
+
+
+// //arroe
+
+// let greet = () => {
+//     console.log("im rosh")
+//     return 9
+// }
+
+// console.log(greet())
+
+//par local variable
+
+// let greet3 = function(user){
+//     console.log("rosh "+user)
+//     return 9
+// }
+
+// console.log(greet3("love pop"))
+
+
+// let greet3 = (user) =>{
+//     console.log("rosh "+user)
+//     return 9
+// }
+
+// console.log(greet3("love pop"))
+
+//aduth try
+
+// let add = (num1,num2) => num1+num2
+
+// let sum = add;
+// let result = sum (78,89)
+// console.log(result);
+
+
+////home work self
+
+
+// let prem = (a,b) => a*b;
+// let pop = prem
+// let result = pop(13,13)
+// console.log(result)
+
+
+
+let prem = (a,b) => -(a+b);
+let pop = prem
+let result = pop(13,13)
+console.log(result)
