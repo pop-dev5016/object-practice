d217fe2994ce49cb85c82da34263955c73af1cec
devpop <impopcity@gmail.com>
2022-09-21 20:18:13 +0530

    get value

--- js clases.js
+++ js clases.js
@@ -6,7 +6,12 @@ class Pizza{
         this.crust = "original";
         this.type=pizztype;
     }
-
+    get piccacrust(){
+        return this.crust
+    }
+    set piccacrust(piccacrust){
+        this.crust = piccacrust;
+    }
     bake(){
         console.log(`baking a ${this.size} ${this.crust} ${this.type} crust pizza.`)
 
@@ -14,7 +19,7 @@ class Pizza{
 }
 
 const myPizza = new Pizza("chicken kari","10 meter width")
-myPizza.type="supereme";
+myPizza.type="supereme";///dont use this way
+myPizza.piccacrust = "sausage"
 myPizza.bake();
-
-console.log(myPizza.type);
+console.log(myPizza.piccacrust);
