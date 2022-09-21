f0460f84fd7443eea4925d44aaca28dcd8ad877d
devpop <impopcity@gmail.com>
2022-09-21 20:11:22 +0530

    passing

--- js clases.js
+++ js clases.js
@@ -1,16 +1,20 @@
 //js class
 
 class Pizza{
-    constructor(){
-        this.size="medium";
+    constructor(pizztype,pizzasize){
+        this.size=pizzasize;
         this.crust = "original";
+        this.type=pizztype;
     }
 
     bake(){
-        console.log(`baking a ${this.size} ${this.crust} crust pizza.`)
+        console.log(`baking a ${this.size} ${this.crust} ${this.type} crust pizza.`)
 
     }
 }
 
-const myPizza = new Pizza
+const myPizza = new Pizza("chicken kari","10 meter width")
+myPizza.type="supereme";
 myPizza.bake();
+
+console.log(myPizza.type);
